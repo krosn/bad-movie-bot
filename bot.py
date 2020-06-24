64ad0f69ef04cea54c177576bc57e543c54e42ed
karsonic <karsonic7283@gmail.com>
2020-06-23 19:12:00 -0700

    Create my own app

--- bot.py
+++ bot.py
@@ -1,11 +1,24 @@
 import discord
+import json
 
-class MyClient(discord.Client):
+class RsynClient(discord.Client):
+    def __init__(self):
+        super().__init__()
+        self.secrets = RsynClient.load_secrets()
+
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
-client = MyClient()
-client.run('my token goes here')
+    @staticmethod
+    def load_secrets():
+        with open('secrets.json', 'r') as secrets_file:
+            secrets_json = secrets_file.read()
+            return json.loads(secrets_json)
+
+    def run(self):
+        super().run(self.secrets['clientSecret'])
+        print(self.secrets['clientSecret'])
+        
---
+++ run.py
@@ -0,0 +1,5 @@
+from bot import RsynClient
+
+if __name__ == "__main__":
+    client = RsynClient()
+    client.run()
